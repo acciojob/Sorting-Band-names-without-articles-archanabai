4e4547c7c3279af7fb75e600c138a6019bbf053b
archanabai <121252186+archanabai@users.noreply.github.com>
2023-10-18 23:46:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,7 @@
 let arr = ['Tajmahal', 'The Virupaksha Temple', 'Victoria Memorial'];
 
 function getArticleLessString(input) {
-    // example: input: "The tajmahal an umberilla" , output: "tajmahal umberiall"
-
-    // words = "The tajmahal an umberilla".split(" ") = [ "The", "tajmahal", "an", "umberilla" ] 
-    // nonArticleWords = [ "tajmahal", "umberilla"].join(" ") => "tajmahal umberilla"
-
+    
     let words = input.split(" ");
     let nonArticleWords = words.filter(
         // words = [ "The", "tajmahal", "an", "umberilla" ] 
